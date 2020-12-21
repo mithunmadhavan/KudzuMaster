888d507baa06559eac13d969eea6d69aaa5181e2
mithunmadhavan <development.mithun@gmail.com>
2020-12-21 13:33:30 +0530

    Food order

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import RootComponent from './components/RootComponent'
 
 // Daily progress
 import CountryTasteChecker from './screens/Day3/CountryTasteChecker'
+import FoodList from './screens/Day4/FoodList'
 
 export default function App() {
   return (
     <RootComponent>
-      <CountryTasteChecker />
+      <FoodList />
     </RootComponent>
   )
 }
