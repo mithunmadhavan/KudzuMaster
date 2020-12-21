3f5a7d57b2c8f447726e80c78204f12677f8ad84
mithunmadhavan <development.mithun@gmail.com>
2020-12-21 19:53:13 +0530

    counter example

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import RootComponent from './components/RootComponent'
 
 // Daily progress
-import CountryTasteChecker from './screens/Day3/CountryTasteChecker'
-import FoodList from './screens/Day4/FoodList'
+import CountryTasteChecker from './screens/Day3/CountryTasteChecker';
+import FoodList from './screens/Day4/FoodList';
+import Counter from './screens/Day5/Counter';
 
 export default function App() {
   return (
     <RootComponent>
-      <FoodList />
+      <Counter />
     </RootComponent>
   )
 }
